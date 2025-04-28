9ac695a3cb488e8bc45e844f069c05fff2021b81
Christian Bourjau <christian.bourjau@quantco.com>
2025-04-28 02:42:01 +0200

    Fix tests for np1x

--- tests/test_utf8.py
+++ tests/test_utf8.py
@@ -1,4 +1,4 @@
-# Copyright (c) QuantCo 2023-2024
+# Copyright (c) QuantCo 2023-2025
 # SPDX-License-Identifier: BSD-3-Clause
 
 import numpy as np
@@ -9,6 +9,7 @@ import ndonnx as ndx
 def test_cast():
     def do(npx):
         arr = npx.asarray(["foo", "", "-1", "0", "true", "True", "false", "False"])
-        return arr.astype(npx.bool)
+        # np1x does not provide np.bool
+        return arr.astype(ndx.bool if npx == ndx else bool)
 
     np.testing.assert_array_equal(do(ndx).unwrap_numpy(), do(np), strict=True)
