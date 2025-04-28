e831b6e61b37da8ff890e14d2e2f55aaee552294
Christian Bourjau <christian.bourjau@quantco.com>
2025-04-28 11:24:45 +0200

    Skip test on np1x

--- tests/test_utf8.py
+++ tests/test_utf8.py
@@ -2,14 +2,15 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import numpy as np
+import pytest
 
 import ndonnx as ndx
 
 
+@pytest.mark.skipif(np.__version__ < "2", reason="np1x semantics differ")
 def test_cast():
     def do(npx):
         arr = npx.asarray(["foo", "", "-1", "0", "true", "True", "false", "False"])
-        # np1x does not provide np.bool
-        return arr.astype(ndx.bool if npx == ndx else bool)
+        return arr.astype(npx.bool)
 
     np.testing.assert_array_equal(do(ndx).unwrap_numpy(), do(np), strict=True)
