c13253b66e682d4b0a0bc3a8463d9b143c571b06
Christian Bourjau <christian.bourjau@quantco.com>
2025-05-25 17:36:21 +0200

    Fix tests on NumPy 1.x

--- tests/test_manipulation_functions.py
+++ tests/test_manipulation_functions.py
@@ -1,11 +1,13 @@
-# Copyright (c) QuantCo 2023-2024
+# Copyright (c) QuantCo 2023-2025
 # SPDX-License-Identifier: BSD-3-Clause
 
 import numpy as np
+import pytest
 
 import ndonnx as ndx
 
 
+@pytest.mark.skipif(np.__version__ < "2", reason="NumPy 1.x does not provide 'concat'")
 def test_concat_axis_none_zero_sized():
     def do(npx):
         a1 = npx.zeros(shape=(0, 0), dtype=npx.int8)
