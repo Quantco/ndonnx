7c914243ca4ccb813487ab1fce87a36408336fe6
Sam Gilmour <sam.gilmour@quantco.com>
2026-07-31 00:16:36 +0200

    Add dtype aliases (#225)

--- ndonnx/types.py
+++ ndonnx/types.py
@@ -1,11 +1,11 @@
-# Copyright (c) QuantCo 2023-2025
+# Copyright (c) QuantCo 2023-2026
 # SPDX-License-Identifier: BSD-3-Clause
 
 from __future__ import annotations
 
 from collections.abc import Sequence
 from types import EllipsisType
-from typing import TYPE_CHECKING, TypeAlias, Union
+from typing import TYPE_CHECKING, Literal, TypeAlias, Union
 
 if TYPE_CHECKING:
     from ._array import Array
@@ -27,6 +27,13 @@ SetitemKey: TypeAlias = Union[
 PyScalar = bool | int | float | str
 NestedSequence = Sequence["PyScalar | NestedSequence"]
 
+DTypeAlias: TypeAlias = (
+    (type[bool] | type[int] | type[float])
+    | Literal["bool", "int", "float"]
+    | Literal["int8", "int16", "int32", "int64"]
+    | Literal["uint8", "uint16", "uint32", "uint64"]
+    | Literal["float16", "float32", "float64"]
+)
 
 __all__ = [
     "StrictShape",
